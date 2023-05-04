01f8fbde64cc83dd5863dae83cd9441e9b3745ba
neeleshio <neelesh@campk12.com>
2023-05-04 12:35:20 +0530

    flask server added

---
+++ server/launch_app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# start backend index server
+python3 ./index_server.py &
+echo "index_server running..."
+
+# wait for the server to start - if creating a brand new huge index, on startup, increase this further
+sleep 60
+
+# start the flask server
+python3 ./flask_demo.py &
