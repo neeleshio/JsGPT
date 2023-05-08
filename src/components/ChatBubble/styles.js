4a6f20190e3f57db8bad3170872f6e8df7810477
neeleshio <neelesh@campk12.com>
2023-05-09 01:09:17 +0530

    copy click effect

--- src/components/ChatBubble/styles.js
+++ src/components/ChatBubble/styles.js
@@ -187,9 +187,9 @@ export const StyledAnswerBubble = styled.div`
             }
 
             &.copy__btn {
-                :hover {
+                :active {
                     background-color: #10a37f;
-                    transition: all 0.5s ease-out;
+                    transition: all 0.1s ease-out;
                     transform: scale(0.9);
                 }
             }
